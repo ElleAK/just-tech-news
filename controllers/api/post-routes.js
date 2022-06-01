1dc2ea1c6b959eaa73f5ddfd1233ae0164fdd9c0
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-06-01 14:42:47 -0400

    seeds and updates

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,7 +6,6 @@ const { Post, User, Vote, Comment } = require('../../models');
   // get all users
 router.get('/', (req, res) => {
     Post.findAll({
-    order: [['created_at', 'DESC']],
     attributes: [
           'id', 
           'post_url', 
@@ -94,12 +93,11 @@ router.get('/', (req, res) => {
   // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
+  Post.upvote({...req.body, user_id: req.session.user_id}, { Vote, Comment, User })
     .then(updatedPostData => res.json(updatedPostData))
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
-            
         });
 });
 
