ac7f1f0988940629754939c33f1c527c46dda322
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-06-04 20:28:22 -0400

    logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -66,13 +66,13 @@ router.post('/', (req, res) => {
       password: req.body.password
     })
     .then(dbUserData => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedin = true;
+
       res.json(dbUserData);
-    })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    });
+  })
 
 router.post('/login', (req, res) => {
 //expects {email: 'lernantino@gmail.com', password: 'password1234'}
@@ -90,11 +90,28 @@ User.findOne({
           res.status(400).json({ message: 'Incorrect password!' });
           return;
         }
+
+        req.session.save(() => {
+          // declare session variables
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
         
         res.json({ user: dbUserData, message: 'You are now logged in!' });
-
-}); 
+    }); 
+  });
 });
+//logout route
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
